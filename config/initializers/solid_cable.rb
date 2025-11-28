46d66b9078092b3bbe8c7d3855f003b1d4dedd86
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2025-11-28 03:02:43 +0100

    database

--- config/initializers/solid_cable.rb
+++ config/initializers/solid_cable.rb
@@ -1,6 +1,5 @@
-# config/initializers/solid_cable.rb
 Rails.application.config.after_initialize do
   if defined?(SolidCable::Record)
-    SolidCable::Record.connects_to database: { writing: :production }
+    SolidCable::Record.connects_to database: { writing: :primary_production, reading: :primary_production }
   end
 end
